cc945a9d85a92d23d8ca7c397a9f4e9215b08f16
v1nn1ty <v1nn1ty.add@gmail.com>
2024-03-20 13:53:01 +0300

    fixed one miss

--- app/oauth2.py
+++ app/oauth2.py
@@ -10,7 +10,7 @@ from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-SECRET_KEY = settings.SECRET_KEY
+SECRET_KEY = settings.secret_key
 ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
