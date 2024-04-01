33ac2e953b06fce9805b2d273dda8433b4340a8d
v1nn1ty <v1nn1ty.add@gmail.com>
2024-04-01 11:30:36 +0300

    start writting tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,49 @@
+import pytest
+from fastapi import Depends
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
 from app.main import app
+from app.database import get_db
 
 client = TestClient(app)
+
+
+
+SQL_ALCHEMY_DATABASE_URL = (
+    f"postgresql:"
+    f"//{settings.database_username}"
+    f":{settings.database_password}"
+    f"@{settings.database_hostname}"
+    f":{settings.database_port}"
+    f"/{settings.database_name}"
+)
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+async def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def test_root():
